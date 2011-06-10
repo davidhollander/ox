02d52e1d9af21dc65f03290d6c9b81b09d9cd20b
David Hollander <dhllndr@gmail.com>
2011-06-10 01:30:24 -0500

    Cleaning index

--- scratch/lib.lua
+++
@@ -1,9 +0,0 @@
-local function partial(f,...)
-    local args=...
-    return function(...)
-        for i,a in ipairs(arg) do table.insert(args) end
-        f(unpack(args))
-    end
-end
-
-
--- scratch/testserv.lua
+++
@@ -1,5 +0,0 @@
---xserv.lua
-local ox=require'ox'
-ox.makeserver(8080,ox.accepthttp)
-ox.makeserver(8888,ox.acceptworker)
-ox.loop()
