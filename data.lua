b53a3ebad8378a25a1c6c0da3cd5722b629761b3
David Hollander <dhllndr@gmail.com>
2011-06-14 16:49:33 -0500

    data.cache->data.cache_single, adding data.cache_table and symmetrical data.pool* functions for pooling requests without cacheing responses. Cache single has detailed tests, others still need tests added.

--- data.lua
+++ data.lua
@@ -9,20 +9,17 @@ module(... or 'ox.data',package.seeall)
 
 -- pools asynchronous requests and caches response
 -- update: function(key, cb) to be run when stale
--- ensures update is not run simultaneously for the same key
+--[[ ensures update is not run simultaneously for the same key
 function cache(update)
   local data={}
   local status={}
   local waiting={}
   return function(key, cb)
-    print('Getting '..key..' from cache')
-    print(data[key], status[key], waiting[key] and #waiting[key])
     if status[key] then cb(data[key])
     else
       if status[key]==nil then
         status[key]=false
         update(key, function(val)
-          print('update done, got '..val)
           data[key]=val
           status[key]=true
           for i,fn in ipairs(waiting[key]) do fn(val) end
@@ -34,3 +31,94 @@ function cache(update)
     end
   end
 end
+]]
+
+-- pools requests for update(cb)
+function pool_single(update)
+  local waiting=false
+  return function(cb)
+    if waiting then ti(waiting, cb)
+    else
+      waiting={cb}
+      update(function(val)
+        for i,fn in ipairs(waiting) do fn(val) end
+        waiting=false
+      end)
+    end
+  end
+end
+-- pools requests for update(key, cb)
+function pool_table(update)
+  local waiting={}
+  return function(key, cb)
+    if waiting[key] then ti(waiting[key], cb)
+    else
+      waiting[key]={cb}
+      update(function(val)
+        for i,fn in ipairs(waiting) do fn(val) end
+        waiting[key]=nil
+      end)
+    end
+  end
+end
+-- pools requests for update(cb) and caches response for [timeout] duration
+-- return lambda(cb)
+function cache_single(update, timeout)
+  local value
+  local time=0
+  local waiting=false
+  return function(cb)
+    print(waiting,time,value)
+    if not cb then time=0
+    elseif time>os.time() then return cb(value)
+    elseif waiting then ti(waiting, cb)
+    else
+      waiting={cb}
+      update(function(val)
+        value=val
+        time=os.time()+timeout
+        local waiting_old=waiting
+        waiting=false
+        for i,fn in ipairs(waiting_old) do fn(val) end
+      end)
+    end
+  end
+end
+
+-- pools requests for update(key, cb) and caches response for [timeout] duration
+-- return lambda(key, cb)
+function cache_table(update, timeout)
+  local values={}
+  local times={}
+  local waiting={}
+  return function(key, cb)
+    if not cb then times[key]=nil; values[key]=nil
+    elseif times[key] and times[key]>(os.time()-timeout) then cb(values[key])
+    elseif waiting[key] then ti(waiting[key], cb)
+    else
+      waiting[key]={cb}
+      update(function(val)
+        values[key]=val
+        times[key]=os.time()
+        for i,fn in ipairs(waiting) do fn(val) end
+        waiting[key]=nil
+      end)
+    end
+  end
+end
+
+--[[
+function quorum(t,R,N,fn)
+end
+
+--  form a quorum for t[n](key,cb) that callback with a value
+function quorum_get(t,R,N)
+  return function(val)
+  end
+end
+
+-- form a quorum for t[n](key,val,cb) that callback with a boolean
+function quorum_put(t,R,N)
+  return function(val)
+  end
+end]]
---
+++ tests/data_cache_single.lua
@@ -0,0 +1,64 @@
+local core = require 'ox.core'
+local data = require 'ox.data'
+local nixio = require'nixio'
+
+local count=0
+local timeout=2
+local my_cache_single = data.cache_single(function(cb)
+  print 'computing update in fork...'
+  count=count+1
+  core.call_fork(function() nixio.nanosleep(1); return count end, cb)
+end, timeout)
+
+print 'fetch from cache 10 times'
+
+local results={}
+local j=0
+for i=1,10 do
+  my_cache_single(function(val)
+    j=j+1
+    results[i]=val
+    if j==10 then return test2() end
+  end)
+end
+
+function test2()
+  print 'wait for cache to timeout and repeat'
+  nixio.nanosleep(timeout+1)
+  local k=0
+  for i=11,20 do
+    my_cache_single(function(val)
+      k=k+1
+      results[i]=val
+      if k==10 then return assert_stuff() end
+    end)
+  end
+end
+
+function assert_stuff()
+  print('asserting stuff')
+  print(results[1],results[11])
+  for i=2,10 do
+    assert(results[i]==results[1],'first 10 fetches were inconsistent!')
+  end
+  for i=12,20 do
+    assert(results[i]==results[11],'second 10 fetches were inconsistent!')
+  end
+  assert(count==2,'The cache was updated '..count..' times instead of 2!')
+  print 'get without updating'
+  my_cache_single(function(val)
+    print(val)
+    assert(count==2, 'The cache should still have only updated twice: '..count)
+  end)
+  print 'manually expire'
+  my_cache_single()
+  print 'get and update'
+  my_cache_single(function(val)
+    print(val)
+    assert(count==3, 'The cache should have updated 3 times: '..count)
+  end)
+  core.stop()
+end
+
+core.loop()
+print 'pass.'
